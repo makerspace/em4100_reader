26a62b0883748886e5bd678c897099cb49d47690
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2019-06-04 21:05:04 +0200

    Echoing back name when receiving '?'

--- rfid_main.cpp
+++ rfid_main.cpp
@@ -9,7 +9,6 @@
 em4100_reader::State state;
 uint8_t cardFacility;
 uint32_t cardUid;
-char card_number_str[9];
 
 void setup() {
     Serial.begin(115200);
@@ -21,15 +20,41 @@ void setup() {
     // Enable interrupts and start the state machine.
     em4100.startRfidCapture();
 
-    memset(card_number_str, 0, sizeof(card_number_str));
+    Serial.setTimeout(50);
+}
+
+const char TERMINATOR_CHARS[] = "\n\r";
+bool read_input(char *str_buffer, int buffer_size) {
+    if (Serial.available()) {
+      int chars_read = Serial.readBytes(str_buffer, buffer_size-1);
+
+      // Check that it ends with a terminator char
+      if (strchr(TERMINATOR_CHARS, str_buffer[chars_read-1]) != NULL) {
+          str_buffer[chars_read-1] = '\0'; // Remove termination char
+          return true;
+      }
+    }
+    return false;
 }
 
 void loop() {
+    char card_number_str[9];
     em4100.processCaptureBuffer();
     state = em4100.getState();
 
     if (state == em4100_reader::ERROR_BUFFER_OVERFLOW) {
         Serial.println("ERROR: Buffer overflow!");
+        em4100.startRfidCapture();
+    }
+
+    char read_str[20];
+    // Read command
+    if (read_input(read_str, sizeof(read_str))) {
+        if (strcmp(read_str, "?") == 0) {
+            Serial.println("EM4100 Reader");
+        }
+
+        // Restart capturing again since we screwed with timing
         em4100.startRfidCapture();
     }
 
